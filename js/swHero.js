ac071065dbfa74d9842a451f5e46e0eb35b13471
woosang1120 <bobting000000@naver.com>
2026-09-07 16:41:49 +0900

    스와이퍼개많이만들어서너무지친다like땡벌

--- js/swHero.js
+++ js/swHero.js
@@ -1,4 +1,14 @@
+const mainVisualBg = document.querySelector(
+  ".hero .main-visual .main-visual-bg",
+);
+// console.log(mainVisualBg);
+
 const swHero = new Swiper(".sw-hero", {
+  speed: 1000,
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -6,6 +16,19 @@ const swHero = new Swiper(".sw-hero", {
   navigation: {
     nextEl: ".hero-next-btn",
     prevEl: ".hero-prev-btn",
+  },
+
+  on: {
+    slideChangeTransitionStart: function (swiper) {
+      // const activeSlide = swiper.activeIndex + 1;
+      // console.log(activeSlide);
+      // mainVisualBg.style.backgroundImage = `url("../assets/images/slide_${activeSlide}.png")`;
+      const activeSlide = swiper.slides[swiper.activeIndex];
+      // console.log(activeSlide);
+      const activeSlideImage = activeSlide.querySelector(".slide-image img");
+      // console.log(activeSlideImage.src);
+      mainVisualBg.style.backgroundImage = `url(${activeSlideImage.src})`;
+    },
   },
 });
 // new 함수 호출하는데 연산자 new 써서 호출하면 객체{}를 생성해준다? => 앞글자 대문자는 생성자 함수
